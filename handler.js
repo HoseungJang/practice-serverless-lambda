24b582dd42a554f487df90a469e77a397f7b4e1e
HoseungJang <hsjang.dev@gmail.com>
2020-06-12 19:18:37 +0900

    UPDATE: express proxy

--- handler.js
+++ handler.js
@@ -1,18 +1,5 @@
-'use strict';
+const awsServerlessExpress = require("aws-serverless-express");
+const app = require("./app");
+const server = awsServerlessExpress.createServer(app)
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+module.exports.api = (event, context) => awsServerlessExpress.proxy(server);
