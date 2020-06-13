575fc5f159489b5d383a9a3627febd5071bde056
HoseungJang <hsjang.dev@gmail.com>
2020-06-13 17:21:32 +0900

    ADD: path validator

--- practice-nodejs-typescript/handler.ts
+++ practice-nodejs-typescript/handler.ts
@@ -1,12 +1,28 @@
 import { Handler, APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
 const hello: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: "Hello?"
-        })
-    };
+    if (event.path === "/hello/world") {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: "Hello, world!"
+            })
+        };
+    } else if (event.path === "/hello") {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: "Hello?"
+            })
+        };
+    } else {
+        return {
+            statusCode: 404,
+            body: JSON.stringify({
+                message: "Not Found"
+            })
+        };
+    }
 };
 
 export { hello };
